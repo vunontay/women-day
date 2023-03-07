1ea59a36c06cf38d61088b8e13b4129ed218843a
github_vunontay <hoangvu200202@gmail.com>
2023-03-07 10:40:48 +0700

    upload source code

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = "Diễm Phúc💘";
+const giftUrl = "";
+const eventName = "Chúc mừng 8-3";
+const titleCard = "Tặng em";
+const contentCard =
+  "Chúc em 8-3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau.";
+
+const giftImage = "hot-girl.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
